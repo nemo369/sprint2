16fcbb267f157d887fe8bee04299181080d3ffaf
Nemo Frenkel <nafrenkel@gmail.com>
2017-11-16 16:10:22 +0200

    basic init

--- js/main.js
+++ js/main.js
@@ -1,27 +1,55 @@
-console.log('איציק ונעמן')
+console.log('2nd sprint- naamna& Itsik')
 
 var gCanvas;
 var ctx;
 var gImg;
+var gImgs = [{id: 0, keywords: ['happy'] },   
+            { id: 1, keywords: ['sad'] }     
+            ];
+var gMeme = { selectedImgId: 5, txts: [{ line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }] }
 
 function init() {
     gCanvas = document.getElementById('canvas');
     ctx = gCanvas.getContext('2d');
-    drawOnCanvas();
+    renderPhotos()
 }
 
-function drawOnCanvas() {
-    gImg = new Image();
-    gImg.src = "../img/memes/1-meme.jpg"
+function drawOnCanvas(imgId) {
+    console.log('img', imgId)
+    var elPoolImg = document.querySelector(".imgPool");
+    var elCanvas= document.querySelector("#canvas");
+    elPoolImg.style.display = 'none';
+    elCanvas.style.display = 'flex';
 
-    gImg.onload = function () {
-        ctx.drawImage(gImg, 0, 0, 568, 360);
+    img = new Image();
+    img.src = `../img/memes/${imgId}-meme.jpg`
 
+   img.onload = function () {
+        ctx.drawImage(img, 0, 0, 568, 360);
         ctx.font = "50px 'Segoe UI'";
         ctx.fillStyle = 'white';
-        ctx.fillText("Text on Canvas", 50, 300);
     };
 }
+
+function renderPhotos() {
+    var elImgPool = document.querySelector('.imgPool');
+
+    var strHtmls = gImgs.map(function (img, idx) {
+        return `
+            <div class="singalImg" onclick="drawOnCanvas(${img.id})">
+            <img src="img/memes/${img.id}-meme.jpg" alt=""></div> 
+        `
+    });
+
+
+    elImgPool.innerHTML = strHtmls.join('')
+}
+
+
+
+
+
+
 
 
 
