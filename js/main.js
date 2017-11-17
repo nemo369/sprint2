67bcb5da6a5d7263125ad8b3ea9e84e8520d6a8a
Nemo Frenkel <nafrenkel@gmail.com>
2017-11-17 08:19:45 +0200

    first merging

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ console.log('2nd sprint- naamna& Itsik')
 
 var gCanvas;
 var ctx;
-var gImg;
-var gImgs = [{id: 0, keywords: ['happy'] },   
-            { id: 1, keywords: ['sad'] }     
-            ];
+// TODO add url .
+// TODO function addIMG
+var gImgs = [{ id: 0, name: 'ddd', keywords: ['happy'] },
+    { id: 1, name: 'ddd', keywords: ['sad'] },
+    { id: 2, name: 'ddd', keywords: ['sad'] },
+    { id: 3, name: 'ddd', keywords: ['sad'] },
+    { id: 4, name: 'ddd', keywords: ['sad'] },
+    { id: 5, name: 'ddd', keywords: ['sad'] },
+    { id: 6, name: 'ddd', keywords: ['sad'] },
+    { id: 7, name: 'ddd', keywords: ['sad'] },
+];
 var gMeme = { selectedImgId: 5, txts: [{ line: 'I never eat Falafel', size: 20, align: 'left', color: 'red' }] }
 
 function init() {
@@ -13,18 +20,26 @@ function init() {
     ctx = gCanvas.getContext('2d');
     renderPhotos()
 }
-
+// toggle canvas
+function toggleCanvas(toOpen, imgId) {
+    var elImgPool = document.querySelector(".imgPool");
+    var elCanvas = document.querySelector(".memeGenerator");
+    if (toOpen) {
+        elImgPool.style.display = 'none';
+        elCanvas.style.display = 'flex';
+        drawOnCanvas(imgId)
+    }   else    {
+        elImgPool.style.display = 'flex';
+        elCanvas.style.display = 'none';  
+        // renderPhotos()
+    }
+}
 function drawOnCanvas(imgId) {
-    console.log('img', imgId)
-    var elPoolImg = document.querySelector(".imgPool");
-    var elCanvas= document.querySelector("#canvas");
-    elPoolImg.style.display = 'none';
-    elCanvas.style.display = 'flex';
 
     img = new Image();
     img.src = `../img/memes/${imgId}-meme.jpg`
 
-   img.onload = function () {
+    img.onload = function () {
         ctx.drawImage(img, 0, 0, 568, 360);
         ctx.font = "50px 'Segoe UI'";
         ctx.fillStyle = 'white';
@@ -36,7 +51,7 @@ function renderPhotos() {
 
     var strHtmls = gImgs.map(function (img, idx) {
         return `
-            <div class="singalImg" onclick="drawOnCanvas(${img.id})">
+            <div class="singalImg" onclick="toggleCanvas(true, ${img.id})">
             <img src="img/memes/${img.id}-meme.jpg" alt=""></div> 
         `
     });
