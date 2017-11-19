35d886a8ce31b3edc787d662829d726633b0873d
itsikben <itsikba5@gmail.com>
2017-11-19 18:07:48 +0200

    itsik

--- js/generatorFueturs.js
+++ js/generatorFueturs.js
@@ -5,8 +5,6 @@ function rendeGneratorPanel() {
     var strHtml = ''
     for (var i = 0; i < gMeme.txts.length; i++) {
         strHtml += `<input type="text" id="textToCanvas" name="t" placeholder="Insert meme" onkeyup="getTxt(this.value, ${i})">
-        <input type="number" value="50" id="font-size" name="t" placeholder="font size" onchange="getFontSize(this.value,${i})">
-        <input type="color"  id="color-picker" onchange="getColor(this.value,${i})">
         <div class="button-bar">
             <button onclick="txtDirection('left',${i}, 20)"><i class="fa fa-align-left" aria-hidden="true"></i></button>
             <button onclick="txtDirection('right',${i},400)"><i class="fa fa-align-right" aria-hidden="true"></i></button>
@@ -14,6 +12,8 @@ function rendeGneratorPanel() {
             <button onclick="isTextShadow()" class="shadow">shadow</button>
             <button onclick="textUp(10,0)" ><i class="fa fa-arrow-up" aria-hidden="true"></i></button>
             <button onclick="textDown(10,0)"><i class="fa fa-arrow-down" aria-hidden="true"></i></button>
+            <button><input type="color"  id="color-picker" onchange="getColor(this.value,${i})"></button>
+            <button>  <input type="number" value="50" id="font-size" name="t" placeholder="font size" onchange="getFontSize(this.value,${i})"></button>
         </div>`
     }
     elGeneratorPanel.innerHTML = strHtml;
